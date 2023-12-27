feadab079c5b01d256c067477a90a24dcc3da54a
Made Indra <made.indra@protonmail.com>
2023-12-27 22:14:53 +0700

    added fastify multipart

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // import modules
 import fastify from 'fastify'
+import { fastifyMultipart } from '@fastify/multipart'
 
 // import utilities
 import { getHost, getPort } from './utils/env'
@@ -8,8 +9,11 @@ const app = fastify({ logger: true })
 
 // initialize fastify with plugins
 async function init (): Promise<void> {
-  app.get('/', (_request, reply) => {
-    return reply.send({ status: 'ok' })
+  await app.register(fastifyMultipart, {
+    limits: {
+      files: 1, // limit to 1 file
+      fileSize: 1024 * 1024 * 5 // limit size to 5 MB
+    }
   })
 
   await app.ready()
@@ -17,6 +21,25 @@ async function init (): Promise<void> {
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 init()
+
+// routes definition
+app.get('/', (_request, reply) => {
+  return reply.send({ status: 'ok' })
+})
+
+app.post('/', async (req, reply) => {
+  const data = await req.file()
+  if (data === undefined) {
+    return await reply.send({ status: 'error', message: 'No file uploaded' })
+  }
+
+  try {
+    const buffer = await data.toBuffer()
+    return await reply.send({ buffer })
+  } catch (err) {
+    return await reply.send({ status: 'error', message: 'File size exceeded the limit of 5MB' })
+  }
+})
 
 // start the server
 async function start (): Promise<void> {
